c553a5abd7bed4e18c44b3f58fea58f71e4c5b81
Manuel Izurieta <manuel@aaa.aaa>
2017-06-28 22:37:43 -0400

    Initial commit with app

---
+++ src/main/java/org/test/javadesignpatterns/App.java
@@ -0,0 +1,13 @@
+package org.test.javadesignpatterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
